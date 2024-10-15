0948d043e73e4792c5ef13bd1009b3c593ff6020
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2024-10-15 10:41:59 -0300

    Initial commit

---
+++ src/main/java/com/pierandrei/bludit/BluditApplication.java
@@ -0,0 +1,13 @@
+package com.pierandrei.bludit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BluditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluditApplication.class, args);
+	}
+
+}
